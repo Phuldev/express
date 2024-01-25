f00075816c26eef4abd26f8e1d2c71ff5331f3b6
DwashKarma <dwaskc@gmail.com>
2024-01-25 10:26:59 +0545

    feat:moongoose connect

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,17 +1,29 @@
-exports.createInfo = (req, res) => {
-    try{
-        const dummyData = {
-            name: "Phuldev Mandal",
-            address: "Kathmandu,Nepal",
-            age: "24",
-            email: "phuldevmandal20@gmail.com",
-            mobileNumber: "9817348212",
-          };
-          res.status(200).json(dummyData);
-    }
-    catch(err){
-        res.startus(400).json({
-            message:"somthing errors"
-        })
-    }
+const { Mongo } = require("../model/userScheme");
+
+const createInfo = async (req, res) => {
+  try {
+    // const dummyData = {
+    //     name: "Phuldev Mandal",
+    //     address: "Kathmandu,Nepal",
+    //     age: "24",
+    //     email: "phuldevmandal20@gmail.com",
+    //     mobileNumber: "9817348212",
+    //   };
+    const DB = await Mongo.create(req.body);
+    res.status(200).json({ DB });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({
+      error: err.message,
+    });
+  }
+};
+
+const getInfo = async (req, res) => {
+  const Db = await Mongo.find({});
+  res.status(400).json({ Db });
+};
+module.exports = {
+  createInfo,
+  getInfo,
 };
